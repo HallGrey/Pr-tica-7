0a177071f182ffdd354dc690576f95c2b2fed184
HallGrey <murilo.dossantosmarcelino@gmail.com>
2023-05-22 21:40:16 -0300

    Pratica Sete de Back-End

---
+++ ex1.js
@@ -0,0 +1,11 @@
+// 1. Criar um array e percorrê-lo utilizando o loop for
+// a. Crie um array com o nome "nomes" e adicione 4 nomes de
+// pessoas que você conhece
+// b. Utilize o loop for para percorrer o array e exibir os nomes na
+// tela
+
+let nomes = ["Vanágila", "Willian", "Richardson", "João"]
+
+for (let i = 0; i <= nomes.length - 1; i++){
+    
+}
